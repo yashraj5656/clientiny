21c351b21d02d197559819cff5784a73122a2409
yashraj5656 <yashrajsingh3876@gmail.com>
2025-06-14 12:35:56 +0530

    commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,19 +6,18 @@ function Home() {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    axios.get('https://inyserver-2.onrender.com/articles')
-      .then((res) => setArticles(res.data))
-      .catch((err) => console.error('Error fetching articles:', err));
+    axios.get('https://inyserver-2.onrender.com/articles').then((res) => {
+      setArticles(res.data);
+    });
   }, []);
 
   return (
-    <div>
-      {articles.map((article) => (
-        <div key={article._id} className="article-preview">
-          <h2>{article.title}</h2>
-          <p>By {article.author}</p>
-          <p>{article.content.slice(0, 150)}...</p>
-          <Link to={`/article/${article._id}`}>Read Full Article</Link>
+    <div className="articles">
+      {articles.map((a) => (
+        <div key={a._id} className="article-card">
+          <h2><Link to={`/article/${a._id}`}>{a.title}</Link></h2>
+          <p className="meta">By {a.author} • {new Date(a.date).toDateString()}</p>
+          <p>{a.content.slice(0, 200)}...</p>
         </div>
       ))}
     </div>
